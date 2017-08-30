b577773224612f84a9d19c32f8d58360e7e41243
Gazaunga <Gazaunga@gmail.com>
2017-08-30 13:08:50 -0400

    Create prompt.sh

---
+++ prompt.sh
@@ -0,0 +1,7 @@
+# Fancy
+e_underline "Some fancy terminal sugar"
+curl -sLf https://spacevim.org/install.sh | bash
+curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
+cd ~
+git clone https://github.com/nojhan/liquidprompt.git
+source liquidprompt/liquidprompt
