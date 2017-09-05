8b19b4e2a766afc914872d7d1255ff8216ee3874
Gazaunga <Gazaunga@gmail.com>
2017-09-05 12:25:55 -0400

    Create ali.py

---
+++ ali.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+#----------------------------------#
+# ,--. ,--.         ,--.           #
+# |  .'   / ,---. ,-'  '-. ,--,--. #
+# |  .   ' | .-. |'-.  .-'' ,-.  | #
+# |  |\   \' '-' '  |  |  \ '-'  | #
+# `--' '--' `---'   `--'   `--`--' #
+#            kotajacob.tk          #
+# Copyright (C) 2017  Dakota Walsh #
+#----------------------------------#
+
+import os
+
+# Open the config file - this is where I keep my alias's
+f = open(os.path.expanduser('~/.bashrc'), 'r')
+
+raw_file = f.readlines()
+for line in raw_file:
+    if line[:5] == "alias":
+        print (line[:len(line)-1])
+
+f.close()
